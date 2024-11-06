54de725b9eda07e6f4a23846266ee3c0538f4ca1
DinhHieu07 <168016302+DinhHieu07@users.noreply.github.com>
2024-11-06 09:19:42 +0700

    Add register/login

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 export const ConnectDB = async() => {
     try {
-        const connect = mongoose.connect(process.env.MONGO_URI)
+        const connect = mongoose.connect(process.env.MONGO_URI);
         console.log("Successfully connect to database")
     } catch (error) {
         console.log("Failed to connect to database")
--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,27 @@
 import express from "express"
 import dotenv from "dotenv"
 import { ConnectDB } from "./config/db.js"
+import router from "./routes/authRoutes.js"
+
 
 const app = express()
+app.use(express.json())
 const PORT = process.env.PORT || 5000
 
 dotenv.config()
 
 console.log(process.env.MONGO_URI)
 
-app.use((req, res) => {
-    console.log("Requesting ", req)
-    console.log("Responding ", res)
-})
+// app.use((req, res) => {
+//     console.log("Requesting ", req)
+//     console.log("Responding ", res)
+// })
 
-app.get('/', (req, res) => {
-    res.send()
-})
+app.use('/api', router);
+
+// app.get('/', (req, res) => {
+//     res.send()
+// })
 
 app.listen(PORT, () => {
     ConnectDB()
